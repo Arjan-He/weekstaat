77fdd8802d4f5e95708c403335e100fadb42a301
Arjan Hegge <ahegge@home.nl>
2021-05-25 16:56:10 +0200

    uitwerking geeks4geeks

--- tijdschrijven/views.py
+++ tijdschrijven/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import GeeksForm
+from django.forms import formset_factory
 
-# Create your views here.
+
+def geeks(request):
+    context = {}
+
+    # creating a formset and 5 instances of GeeksForm
+    GeeksFormSet = formset_factory(GeeksForm, extra=3)
+    formset = GeeksFormSet(request.POST or None)
+
+    # print formset data if it is valid
+    if formset.is_valid():
+        for form in formset:
+            print(form.cleaned_data)
+
+    # Add the formset to context dictionary
+    context['formset'] = formset
+    return render(request, "geeks.html", context)
--- weekstaat/urls.py
+++ weekstaat/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tijdschrijven import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('geeks', views.geeks, name='geeks')
 ]
